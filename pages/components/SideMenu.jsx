9a49e9f74d863f5d693d965d67ee4b66ae20af78
0Chronic <kyawn1063@gmail.com>
2024-06-29 21:40:25 +0630

    ri

--- pages/components/SideMenu.jsx
+++ pages/components/SideMenu.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
+import { rec_channels , top_channels} from '../data/MOCK_DATA'
+
+import {RiMovieLine} from 'react-icons/ri'
+
+
+
+
+
 const SideMenu = () => {
   return (
-    <div>SideMenu</div>
+    <div className='fixed w-14 sm:w-16 xl::w-[15rem] h-screen bg-[#0e0e10]'>
+      <div>
+        <p>Recommended Channels</p>
+        <p><RiMovieLine /></p>
+     
+      </div>
+
+    </div>
   )
 }
 
---
+++ pages/data/MOCK_DATA.js
@@ -0,0 +1,20 @@
+  
+  
+  
+ export const rec_channels = [{"id":1,"avatar":"https://robohash.org/voluptatumerrorvelit.png?size=50x50&set=set1","username":"bscibsey0","game_name":"Quo Lux","rank":59},
+{"id":2,"avatar":"https://robohash.org/veniamquamillum.png?size=50x50&set=set1","username":"edurbann1","game_name":"Aerified","rank":77},
+{"id":3,"avatar":"https://robohash.org/quamautconsequatur.png?size=50x50&set=set1","username":"dliddon2","game_name":"Tresom","rank":92},
+{"id":4,"avatar":"https://robohash.org/similiquevelitaut.png?size=50x50&set=set1","username":"bcastle3","game_name":"Domainer","rank":67},
+{"id":5,"avatar":"https://robohash.org/voluptatesnoneum.png?size=50x50&set=set1","username":"hdimitru4","game_name":"Cardguard","rank":78},
+{"id":6,"avatar":"https://robohash.org/quiaitaquein.png?size=50x50&set=set1","username":"jkirman5","game_name":"Temp","rank":41},
+{"id":7,"avatar":"https://robohash.org/temporibusporrout.png?size=50x50&set=set1","username":"rschukert6","game_name":"Andalax","rank":86},
+{"id":8,"avatar":"https://robohash.org/abeossoluta.png?size=50x50&set=set1","username":"predpath7","game_name":"Redhold","rank":37},
+{"id":9,"avatar":"https://robohash.org/mollitiaetvoluptas.png?size=50x50&set=set1","username":"wmcravey8","game_name":"Bitchip","rank":43},
+{"id":10,"avatar":"https://robohash.org/nostrummodivel.png?size=50x50&set=set1","username":"lgreg9","game_name":"Tampflex","rank":100}]
+
+
+export const top_channels = [{"id":1,"avatar":"https://robohash.org/voluptatumerrorvelit.png?size=50x50&set=set1","username":"bscibsey0","game_name":"Quo Lux","rank":59},
+    {"id":2,"avatar":"https://robohash.org/veniamquamillum.png?size=50x50&set=set1","username":"edurbann1","game_name":"Aerified","rank":77},
+    {"id":3,"avatar":"https://robohash.org/quamautconsequatur.png?size=50x50&set=set1","username":"dliddon2","game_name":"Tresom","rank":92},
+    {"id":4,"avatar":"https://robohash.org/similiquevelitaut.png?size=50x50&set=set1","username":"bcastle3","game_name":"Domainer","rank":67},
+  ]
