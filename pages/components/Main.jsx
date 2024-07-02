02c29f983f5467fefffff0a10e6843d0710029bc
0Chronic <kyawn1063@gmail.com>
2024-07-02 22:07:57 +0630

    video

--- pages/components/Main.jsx
+++ pages/components/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import LiveChannel from './LiveChannel'
 import LiveChannelItem from './LiveChannelItem'
+import Hero from './Hero'
 
 const Main = () => {
   return (
     <div className='absolute left-[64px] xl:left-[220px]'>
+      <Hero />
       <LiveChannel />
      
     </div>
