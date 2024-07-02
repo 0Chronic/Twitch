6765ca1f80ffafec777e8148118824225d39ac1d
0Chronic <kyawn1063@gmail.com>
2024-07-02 22:26:34 +0630

    Iconbar

--- pages/components/Main.jsx
+++ pages/components/Main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import LiveChannel from './LiveChannel'
 import LiveChannelItem from './LiveChannelItem'
 import Hero from './Hero'
+import IconBar from './IconBar'
 
 const Main = () => {
   return (
     <div className='absolute left-[64px] xl:left-[220px]'>
       <Hero />
       <LiveChannel />
+      <IconBar />
      
     </div>
   )
